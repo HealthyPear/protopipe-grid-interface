d44630ca62c7100c0542298cd087f2a2f557c71f
Michele Peresano <michele.peresano@cea.fr>
2020-10-03 18:57:02 +0200

    Update example scripts path

--- example_download_and_merge.sh
+++ example_download_and_merge.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 # User variables
-CONFIG="myconfig"  # Name of the configuration
+ANALYSIS_NAME=""  # Name of the analysis
 TYPE="dl2"  # Here dl1 or dl2
 FILE_TYPE="dl2"  # Here dl1_energy, dl1_discrimination, dl2, dl2_force_tc_extended_cleaning
 MODE="tail"  # Here, tail, wave
-PARTICLE='gamma '  # This a list, gamma, proton, electron
+PARTICLE='gamma '  # This can be list, gamma, proton, electron
 
-# For Python's script
-INPUT_DIR="/vo.cta.in2p3.fr/user/x/xxx/path_to_my_config/$CONFIG/$FILE_TYPE"
-OUTPUT_DIR="path_to_my_config/$CONFIG/$FILE_TYPE/"
+# DIRAC file catalog full path
+INPUT_DIR="/vo.cta.in2p3.fr/user/x/xxx/path_to_analysis/$ANALYSIS_NAME/$FILE_TYPE"
+# Full path in local virtual environment for the grid interface
+OUTPUT_DIR="path_to_my_config/$ANALYSIS_NAME/data/$FILE_TYPE/"
 
 # Get files
-python download_files.py --indir=$INPUT_DIR --outdir=$OUTPUT_DIR
+python $GRID/download_files.py --indir=$INPUT_DIR --outdir=$OUTPUT_DIR
 
 # Merge files
 for part in $PARTICLE; do
