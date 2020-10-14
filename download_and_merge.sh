286ac648d992636fbbb808a9043944aae04dc9c8
Michele Peresano <michele.peresano@.cea.fr>
2020-10-14 13:09:24 +0200

    Fix codacy complaints for download and merge auxiliary script

--- download_and_merge.sh
+++ download_and_merge.sh
@@ -14,15 +14,15 @@ ANALYSIS_PATH_LOCAL="home/vagrant/data/analyses/$ANALYSIS_NAME"
 DATA_PATH="DL1/for_energy_estimation"
 
 MODE="tail"  # Here tail (tailcut) or wave (wavelet) cleaning
-PARTICLE='gamma '  # This can be list up to "gamma, proton, electron"
+PARTICLE="gamma "  # This can be list up to "gamma, proton, electron"
 
 # DIRAC file catalog full path
-INPUT_DIR="$ANALYSIS_PATH_GRID/$ANALYSIS_NAME/data/$DATA_PATH"
+INPUT_DIR="$HOME_PATH_GRID/$ANALYSIS_PATH_GRID/$ANALYSIS_NAME/data/$DATA_PATH"
 # Full path in local virtual environment for the grid interface
-OUTPUT_DIR="home/vagrant/shared_folder/analyses/$ANALYSIS_NAME/data/$DATA_PATH/"
+OUTPUT_DIR="$ANALYSIS_PATH_LOCAL/data/$DATA_PATH/"
 
 # Get files
-python $GRID/download_files.py --indir=$INPUT_DIR --outdir=$OUTPUT_DIR
+python $GRID/download_files.py --indir="$INPUT_DIR" --outdir="$OUTPUT_DIR"
 
 # Merge files
 for part in $PARTICLE; do
@@ -30,9 +30,9 @@ for part in $PARTICLE; do
     OUTPUT_FILE="$OUTPUT_DIR/${TYPE}_${MODE}_${part}_merged.h5"
     TEMPLATE_FILE_NAME="${TYPE}_${MODE}_${part}"
 
-    echo $OUTPUT_DIR
-    echo $TEMPLATE_FILE_NAME
-    echo $OUTPUT_FILE
+    echo "$OUTPUT_DIR"
+    echo "$TEMPLATE_FILE_NAME"
+    echo "$OUTPUT_FILE"
 
-    python $GRID/merge_tables.py --indir=$OUTPUT_DIR --template_file_name=$TEMPLATE_FILE_NAME --outfile=$OUTPUT_FILE
+    python $GRID/merge_tables.py --indir="$OUTPUT_DIR" --template_file_name="$TEMPLATE_FILE_NAME" --outfile="$OUTPUT_FILE"
 done
