ca8381aa1b825472f7bbd2af29fc7817198c63ba
Michele Peresano <michele.peresano@.cea.fr>
2020-10-15 17:17:45 +0200

    Improve readability of download and merge script.

--- download_and_merge.sh
+++ download_and_merge.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
+# ============================================
+#      END USERS SHOULD EDIT ONLY THIS PART
+# ============================================
+
 # User variables
 ANALYSIS_NAME=""
-HOME_PATH_GRID="/vo.cta.in2p3.fr/user/x/xxx/"
-ANALYSIS_PATH_GRID="" # path from HOME_PATH_GRID
-ANALYSIS_PATH_LOCAL="/home/vagrant/shared_folder/analyses/$ANALYSIS_NAME"
+HOME_PATH_GRID="/vo.cta.in2p3.fr/user/x/xxx/" # fill this
+ANALYSIS_PATH_GRID="" # path from HOME_PATH_GRID, if none leave empty
 
 # ANALYSIS STEP DATA PATH
+# Possible choices are,
 # - TRAINING/for_energy_estimation
 # - TRAINING/for_particle_classification
 # - DL2
 DATA_PATH="TRAINING/for_energy_estimation"
 
 MODE="tail"  # Here tail (tailcut) or wave (wavelet) cleaning
+
 PARTICLE="gamma "  # This can be list up to "gamma, proton, electron"
 
 # DIRAC file catalog full path
 INPUT_DIR="$HOME_PATH_GRID/$ANALYSIS_PATH_GRID/$ANALYSIS_NAME/data/$DATA_PATH"
 # Full path in local virtual environment for the grid interface
 OUTPUT_DIR="$ANALYSIS_PATH_LOCAL/data/$DATA_PATH/"
+
+# ============================================
+
+ANALYSIS_PATH_LOCAL="/home/vagrant/shared_folder/analyses/$ANALYSIS_NAME"
+
+# FILE TYPE
+case $rental in
+   "TRAINING/for_energy_estimation") TYPE="TRAINING_energy";;
+   "TRAINING/for_particle_classification") TYPE="TRAINING_classification";;
+   "DL2") TYPE="DL2";;
+esac
 
 # Get files
 python $GRID/download_files.py --indir="$INPUT_DIR" --outdir="$OUTPUT_DIR"
