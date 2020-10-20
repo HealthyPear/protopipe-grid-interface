801ab584e37fcbaf9e0935e16704226c947892e7
Michele Peresano <michele.peresano@.cea.fr>
2020-10-20 18:32:25 +0200

    Improve output of files upload

--- upload_file.py
+++ upload_file.py
@@ -7,25 +7,21 @@ import subprocess
 
 def main():
     # Read command line options
-    parser = argparse.ArgumentParser(description="Upload collection files from Dirac")
+    parser = argparse.ArgumentParser(description="Upload file(s) on the GRID")
     parser.add_argument("--indir", default=None, help="Input repository")
     parser.add_argument("--infile", default=None, help="Input file")
-    parser.add_argument("--outdir", default=None, help="Dirac repository")
+    parser.add_argument("--outdir", default=None, help="GRID repository")
     args = parser.parse_args()
 
     file_to_upload = os.path.join(args.indir, args.infile)
     file_on_dirac = os.path.join(args.outdir, args.infile)
-    print("Upload file: {} to {}".format(file_to_upload, file_on_dirac))
 
     # Get list of files
     batcmd = "dirac-dms-add-file {} {} {}".format(
         file_on_dirac, file_to_upload, "CC-IN2P3-USER"
     )
     result = subprocess.check_output(batcmd, shell=True)
-    file_list = result.split()[-1]
-
-    print("Result of the operation = {}".format(result))
-    print("Files concerned by this operation = {}".format(file_list))
+    print(result)
 
 
 if __name__ == "__main__":
