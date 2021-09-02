01320a0824b2c7b8e1e30f6c84f6dfcccf847bff
Michele Peresano <michele.peresano@cea.fr>
2021-09-02 16:58:18 +0200

    add check in pilot to better debug failed jobs

--- pilot.sh
+++ pilot.sh
@@ -5,4 +5,8 @@ export PATH=$PATH:./
 echo $@ | awk 'BEGIN{RS="&&"}{print}' | while read CMD
 do
     $CMD
+    if [[ ( $? != 0 ) ]]; then
+      echo "Error while executing $CMD"
+      exit -1
+    fi
 done
