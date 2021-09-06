1227c8eb51b678b87c1da76b0f31bd9fb063076f
Michele Peresano <michele.peresano@cea.fr>
2021-09-06 14:31:22 +0200

    revert change

--- create_analysis_tree.py
+++ create_analysis_tree.py
@@ -229,10 +229,8 @@ using the protopipe prototype pipeline.
         # Same with the analysis configuration file
         setup_config(os.path.join(protopipe_configs, "analysis.yaml"),
                      os.path.join(analysis_path, "configs/analysis.yaml"),
-                     ["analyses_directory: ''",
-                      "config_name: ''"],
-                     ["analyses_directory: '{}'".format(analysis_path),
-                      "config_name: '{}'".format(analysis_name)]
+                     ["config_name: ''"],
+                     ["config_name: '{}'".format(analysis_name)]
                      )
 
         # Same with the benchmarks configuration file
