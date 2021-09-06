b3e4b957e4c3a5a591cf4a7b73808e2d1efb45d9
Michele Peresano <michele.peresano@cea.fr>
2021-09-06 10:33:25 +0200

    Improve setup error messages and remove exit commands

--- setup.sh
+++ setup.sh
@@ -5,16 +5,14 @@ export GRID_INTERFACE=$(dirname $GRID_INTERFACE_full_path)
 echo "\$GRID_INTERFACE points to $GRID_INTERFACE"
 
 if [[ -z "${DIRAC}" ]]; then
-  echo "ERROR: DIRAC is not installed or it has not being installed properly."
-  echo "\$DIRAC environment variable undefined"
-  exit
+  echo "ERROR: \$DIRAC environment variable undefined!"
+  echo "Please, make sure that DIRAC has been installed and initialized."
 else
   $DIRAC/diracos/usr/bin/pip install -r "$GRID_INTERFACE/requirements.txt"
 fi
 
 # check if dirac has been initialized
 if [ "$(command -v dirac-info)" != "$DIRAC/scripts/dirac-info" ]; then
-    echo "ERROR: DIRAC is not installed or it has not being installed properly."
-    echo "Command 'dirac-info' could not be found or it is not where is should be..."
-    exit
+    echo "ERROR: DIRAC scripts not accessible."
+    echo "Please, make sure that DIRAC has been properly installed."
 fi
