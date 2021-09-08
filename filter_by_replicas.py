32a40c320b001e50e9e222a4db9e9a2b10bffd44
Michele Peresano <michele.peresano@cea.fr>
2021-09-08 17:33:56 +0200

    improvements
    - add max_file argument to CLI
    - fix conditions
    - change variable names

--- filter_by_replicas.py
+++ filter_by_replicas.py
@@ -25,25 +25,34 @@ def main():
         "--outfile", type=str, required=True,
         help="Full path of the output list"
     )
+    parser.add_argument(
+        "--max_files", type=int, default=None,
+        help="Maximum number of files to scan"
+    )
     args = parser.parse_args()
 
     input_list = args.input_list
     outfile = open(args.outfile, "w")
 
-    with open(args.input_list) as list_of_files:
-        files = list_of_files.readlines()
-        for file in tqdm(files,
+    with open(args.input_list) as list_of_lfns:
+        lfns = list_of_lfns.readlines()[:args.max_files]
+        for lfn in tqdm(lfns,
                          desc="Files processed",
                          total=len(files),
                          unit="file"):
-            output = subprocess.check_output(["dirac-dms-lfn-replicas", file[:-1]])
+
+            output = subprocess.check_output(["dirac-dms-lfn-replicas", lfn[:-1]])
             components = output.split(" ")
+
             disks = []
             for comp in components:
                 if "Disk" in comp:
                     disks.append(comp)
-            if any(disk in disks for disk in args.filter_disks):
-                outfile.writelines(file)
+
+            if ((len(args.filter_disks)==1) and (args.filter_disks == disks)):
+                outfile.writelines(lfn)
+            elif ((len(args.filter_disks)>1) and any(disk in disks for disk in args.filter_disks)):
+                outfile.writelines(lfn)
 
     outfile.close()
 
