a6f77a8c6cf767cc7802999d75e190ee8ca92f06
Michele Peresano <michele.peresano@cea.fr>
2021-09-09 17:16:51 +0200

    print error code before echo is done

--- pilot.sh
+++ pilot.sh
@@ -6,7 +6,7 @@ echo $@ | awk 'BEGIN{RS="&&"}{print}' | while read CMD
 do
     $CMD
     if [[ ( $? != 0 ) ]]; then
-      echo "Error while executing $CMD"
-      exit $?
+      echo "Error while executing $CMD ==========> ERROR CODE: $?"
+      exit -1
     fi
 done
