68b7db1656a536484f711853a04fea509c6b5523
Michele Peresano <michele.peresano@cea.fr>
2021-09-12 14:29:56 +0200

    use command status to exit

--- pilot.sh
+++ pilot.sh
@@ -5,8 +5,9 @@ export PATH=$PATH:./
 echo $@ | awk 'BEGIN{RS="&&"}{print}' | while read CMD
 do
     $CMD
+    status = $?
     if [[ ( $? != 0 ) ]]; then
-      echo "Error while executing $CMD ==========> ERROR CODE: $?"
-      exit -1
+      echo "Error while executing $CMD ==========> ERROR CODE: $status"
+      exit $status
     fi
 done
