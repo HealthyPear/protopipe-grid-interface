52141bdd83ea1b56340f35507eaea7cdf36aa1b9
Michele Peresano <michele.peresano@cea.fr>
2021-09-17 14:27:03 +0200

    Fix wrong env variables when downloading and merging

--- download_and_merge.sh
+++ download_and_merge.sh
@@ -34,7 +34,7 @@ INPUT_DIR="$HOME_PATH_GRID/$ANALYSIS_PATH_GRID/$ANALYSIS_NAME/data/$DATA_TYPE"
 OUTPUT_DIR="$ANALYSIS_PATH_LOCAL/data/$DATA_TYPE"
 
 # FILE TYPE
-case $DATA_PATH in
+case $DATA_TYPE in
   "TRAINING/for_energy_estimation") TYPE="TRAINING_energy";;
   "TRAINING/for_particle_classification") TYPE="TRAINING_classification";;
   "DL2") TYPE="DL2";;
@@ -52,8 +52,8 @@ for part in $PARTICLE; do
 
   # Merge files
   echo "Merging $part..."
-  OUTPUT_FILE="$OUTPUT_DIR/${DATA_TYPE}_${MODE}_${part}_merged.h5"
-  TEMPLATE_FILE_NAME="${DATA_TYPE}_${part}_${MODE}"
+  OUTPUT_FILE="$OUTPUT_DIR/${TYPE}_${MODE}_${part}_merged.h5"
+  TEMPLATE_FILE_NAME="${TYPE}_${part}_${MODE}"
   $DIRAC/diracos/usr/bin/python $GRID_INTERFACE/merge_tables.py --indir="$OUTPUT_DIR" --template_file_name="$TEMPLATE_FILE_NAME" --outfile="$OUTPUT_FILE"
 
 done
