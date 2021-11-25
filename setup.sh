e9768d8b9af9c60fe593cc9180200195f4ea0a32
Michele Peresano <michele.peresano@cea.fr>
2021-11-25 14:59:03 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # were 'realpath' is defined, but just in case...
 if [[ ( "$OSTYPE" == "darwin"* ) && ( -z "$(command -v realpath)" ) ]]; then
   echo "ERROR: realpath command not found!"
-  echo "Please," 
+  echo "Please,"
   echo " - either use the Docker Container,"
   echo " - or install the command using Homebrew (brew install coreutils)."
   return 1
@@ -21,21 +21,20 @@ echo "\$GRID_INTERFACE points to $GRID_INTERFACE"
 export PROTOPIPE="$(dirname $GRID_INTERFACE)/protopipe"
 echo "\$PROTOPIPE points to $PROTOPIPE"
 
-# Check if DIRAC has been initialized
-if [[ -z "${DIRAC}" ]]; then
-  echo "ERROR: \$DIRAC environment variable undefined!"
-  echo "Please, make sure that DIRAC has been installed and initialized."
+# Set VOMS
+export X509_CERT_DIR=$CONDA_PREFIX/etc/grid-security/certificates
+export X509_VOMS_DIR=$CONDA_PREFIX/etc/grid-security/vomsdir
+export X509_VOMSES=$CONDA_PREFIX/etc/grid-security/vomses
+
+# Check that dirac scripts are accessible
+dirac_configure_command_path=$(command -v dirac-configure)
+
+if [ ! $dirac_configure_command_path &> /dev/null ]; then
+  echo "dirac-configure could not be found"
+  echo "DIRAC scripts not accessible."
   return 1
 else
-  $DIRAC/diracos/usr/bin/pip install -r "$GRID_INTERFACE/requirements.txt"
-fi
-
-# This should be also OP, but it can be that the installation has been
-# done in an incorrect way and the scripts are not where they shoud be
-if [ "$(command -v dirac-info)" != "$DIRAC/scripts/dirac-info" ]; then
-    echo "ERROR: DIRAC scripts not accessible."
-    echo "Please, make sure that DIRAC has been properly installed."
-    return 1
+  dirac-configure
 fi
 
 echo "The protopipe GRID interface is ready to be used!"
